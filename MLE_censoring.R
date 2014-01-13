faa22a6b9066edb8612460a6bc17ae2a24fecdd9
PlusTheMonk <dempsey.walter@gmail.com>
2014-01-13 14:51:42 -0600

    Converted Script into a single Function that takes in data and outputs the desired estimates for a specified revival model

--- MLE_censoring.R
+++ MLE_censoring.R
@@ -1,61 +1,34 @@
-setwd("/Users/walterdempsey/Documents/stat/research/joint_models/censoring/jm_censoring/")
-
-# Example Data #
-
-Table_1 = read.table('Table_1_cens')
-Table_2 = read.table('Table_2_cens')
-
-
-# Check the Data for obs_time > survival#
-#throw_away = rep(0,0)
-
-#for(i in 1:(dim(Table_2)[1])) {
-#	throw_away[i] = as.numeric(Table_2$obs_time[i] > Table_1$survival[Table_1$id == Table_2$id[i]])
+revival_model <- function(Table_1, Table_2, X_1, X_2, Sigma_calc, mean_params, cov_params, theta) {
 	
-#}
+	# Fits a Components of Variance Model
 
-#Table_2_cens = Table_2[!throw_away,]
+	# Basic : Assume each patient is iid and take advantage of the block structure
 
-#write.table(Table_2_cens, 'Table_2_cens')
-
-Table_1, Table_2, X_1,X_2, Cov, Sigma_calc, mean_params, cov_params, theta
-
-# Generate the T-Dependent Covariates
-
-X_1 <- function(pat_table) {
-	return(rep(1, dim(pat_table)[1]))	
-}
-
-X_2 <- function(t, pat_table) {
-	# Returns the revival vector
-	return(t- pat_table$obs_times)
-}
-
-# Total Covariates
+	# Structure of the Data
+		# Assume 2 data matrices
+		# Table 1 = Survival Information
+			# Column 1 = id (called 'id')
+			# Column 2 = censored indicator (called 'cens')
+			# Column 3 = survival time (called 'survival')
+		# Table 2 = Longitudinal Observations
+			# Column 1 = id (match Column 1 from Table 1)
+			# Column 2 = Observation time (on the same scale as the survival time) (called 'obs_time')
+			# Column 3 = Observed Value (called 'obs')
+			# Column 4-n = Observed Covariates to Be Used in Fitting Model
 
 Cov <- function(t, pat_table) {
 	return(cbind(X_1(pat_table), X_2(t, pat_table)))
 }
 
-
-
-Sigma_calc <- function(sigmasq_0, sigmasq_1, lambda, pat_table) {
-	# Computes the Covariance Matrix for a Patient
-	
-	return( sigmasq_0 * diag(length(pat_table$obs_times)) 
-	+ sigmasq_1 * exp(-abs(outer(pat_table$obs_times, pat_table$obs_times,"-"))/lambda))
-	
-}
-
-g <- function( beta, sigmasq_0, sigmasq_1, lambda, pat_table) {
+g <- function( mean_params, cov_params, pat_table) {
 	# Provides a function of the survival time, t,
 	# for the likelihood Y | T
 	
 	g2 <- function(t) {
 		k = dim(pat_table)[1]
 		X = Cov(t, pat_table)
-		Sigma = Sigma_calc(sigmasq_0, sigmasq_1, lambda, pat_table)
-		mu = X%*%beta
+		Sigma = Sigma_calc(cov_params, pat_table)
+		mu = X%*%mean_params
 		y = pat_table$obs
 		return( (2*pi)^(-k/2)*det(Sigma)^(-1/2)*exp(-t(y - mu)%*% solve(Sigma) %*% (y - mu)/2) )
 	}
@@ -72,13 +45,13 @@ f <- function(theta) {
 	
 }
 
-h <- function(beta, sigmasq_0, sigmasq_1, lambda, theta, pat_table) {
+h <- function(mean_params, cov_params, theta, pat_table) {
 	# Returns a function that returns value of the joint density 
 	# at T = t given the parameter values
 	
 	h2 <- function(t) {
 		t_dens = f(theta)
-		y_dens = g(beta, sigmasq_0, sigmasq_1, lambda, pat_table) 
+		y_dens = g(mean_params, cov_params, pat_table) 
 		return( t_dens(t) * y_dens(t) )
 	}
 	
@@ -87,11 +60,11 @@ h <- function(beta, sigmasq_0, sigmasq_1, lambda, theta, pat_table) {
 }
 
 
-logint <- function(beta, sigmasq_0, sigmasq_1, lambda, theta, pat_table,c) {
+logint <- function(mean_params, cov_params, theta, pat_table,c) {
 	# Returns the log of the integral from censoring to infty 
 	# of the joint density of (Y,t) 
 	
-	h_vec = Vectorize(h(beta,sigmasq_0,sigmasq_1,lambda,theta,pat_table))
+	h_vec = Vectorize(h(mean_params, cov_params,theta,pat_table))
 	return(log(integrate(h_vec, c, Inf)$value))
 }
 
@@ -106,163 +79,98 @@ log_cdf_T <- function( theta, c) {
 	return(-log(1-cdf_T(theta,c)))
 }
 
-log_lik_cens <- function(beta, sigmasq_0, sigmasq_1, lambda, theta, pat_table,c) {
+log_lik_cens <- function(mean_params, cov_params, theta, pat_table,c) {
 	# Log-likelhood for Censored Patients
-	return( log_cdf_T (theta,c) + logint(beta, sigmasq_0, sigmasq_1, lambda, theta, pat_table,c))
+	return( log_cdf_T (theta,c) + logint(mean_params, cov_params, theta, pat_table,c))
 }
 
-log_lik_uncens <- function(beta, sigmasq_0, sigmasq_1, lambda, theta, pat_table,T) {
+log_lik_uncens <- function(mean_params, cov_params, theta, pat_table,T) {
 	# Log-Likelihood For Uncensored Patients
-	return(log( f(theta)(T) ) + log ( g(beta, sigmasq_0, sigmasq_1, lambda, pat_table)(T)))
+	return(log( f(theta)(T) ) + log ( g(mean_params, cov_params, pat_table)(T)))
 }
 
 ### Complete Log-Likelihood
 
-log_lik <- function(beta, sigmasq_0, sigmasq_1, lambda, theta, table1 = Table_1, table2 = Table_2) {
+log_lik <- function(mean_params, cov_params, theta, table1 = Table_1, table2 = Table_2) {
 	llik = 0
 	for (pat in table1$id) {
 		pat_table = table2[table2$id == pat,]
 		
 		if (table1$cens[table1$id == pat] == 1) {
 			c = table1$survival[table1$id == pat]
-			llik = llik + log_lik_cens(beta, sigmasq_0, sigmasq_1, lambda, theta, pat_table,c)
+			llik = llik + log_lik_cens(mean_params, cov_params, theta, pat_table,c)
 		}
 		if (table1$cens[table1$id == pat] == 0) {
 			T = table1$survival[table1$id == pat]
-			llik = llik + log_lik_uncens(beta, sigmasq_0, sigmasq_1, lambda, theta, pat_table,T)
+			llik = llik + log_lik_uncens(mean_params, cov_params, theta, pat_table,T)
 		}
 	}	
 	return(llik)
 }
 
 log_lik_vector <- function(params, table1 = Table_1, table2 = Table_2) {
-	beta = params[1:2]
-	sigmasq_0 = params[3]
-	sigmasq_1 = params[4]
-	lambda = params[5]
-	theta = params[6]
-	return(log_lik(beta,sigmasq_0, sigmasq_1, lambda,theta, table1, table2))
+	mean_params = params[1:length(mean_params)]
+	cov_params = params[(length(mean_params)+1):length(params)]
+	return(-log_lik(mean_params, cov_params, theta, table1, table2))
 }
-
-# Initialize the Model
-
-censored <- function(x) {Table_1$cens[Table_1$id == x]}
-survival <- function(x) {Table_1$survival[Table_1$id == x]}
-
-Table_2$cens = as.numeric(lapply(Table_2$id, censored))
-Table_2$revival = Table_2$obs_times - as.numeric(lapply(Table_2$id, survival))
-
-Table_2_uncens = Table_2[!Table_2$cens,c(1:3,5)]
-Table_1_uncens = Table_1[!Table_1$cens,]
-
-source('http://www.stat.uchicago.edu/~pmcc/courses/regress.R')
-
-### Compute the Covariance Models
-
-Patient <- outer(Table_2_uncens$id, Table_2_uncens$id, "==")  # Patient Indicator Matrix
-
-baseline_llik <- function(lambda) {
-	cov_lambda <- lambda
-	
-	Patient.ds <- exp(-abs(outer(Table_2_uncens$revival,Table_2_uncens$revival, "-"))/cov_lambda) *Patient # Patient Specific Exponential Covariance Matrix
-
-	baseline_model <- regress(Table_2_uncens$obs~Table_2_uncens$revival, ~Patient.ds, pos = c(1), kernel = 1)
-
-	return(baseline_model$llik)
-
-}
-
-cov_lambda <- 1
-
-Patient.ds <- exp(-abs(outer(Table_2_uncens$revival,Table_2_uncens$revival, "-"))/cov_lambda) *Patient # Patient Specific Exponential Covariance Matrix
-
-baseline_model <- regress(Table_2_uncens$obs~Table_2_uncens$revival, ~Patient.ds, pos = c(1), kernel = 1)
-
-summary(baseline_model)
 
 # Fit from the Initialization Given By Model_Cens
-library(numDeriv)
 
-beta = as.vector(baseline_model$beta)
-sigmasq_0 = baseline_model$sigma[1]
-sigmasq_1 = baseline_model$sigma[2]
-theta = (sum(Table_1$cens))/sum(Table_1$survival)
+inits <- c(mean_params, cov_params, theta)
 
-eps = 0.005
-delta = 0.001
-max_eps = 0.05
+print('Got to The Optimization Component')
 
-step_size = seq(eps,max_eps,delta)
-precision = 0.01
-x_old = c(beta, max(sigmasq_0,0.05), max(sigmasq_1,0.05), cov_lambda, theta)
-x_old
+op_llik <- optim(inits, log_lik_vector,lower = c(rep(-Inf,length(mean_params)), rep(0, length(cov_params)), 0), upper = c(rep(Inf, length(mean_params) + length(cov_params)), 5))
 
-for(iter in 1:100) {
-	fprime = grad(log_lik_vector,x_old)
-	
-	# Line Search	
-	x_new = x_old + eps*c(fprime[1:5],0)
-#	x_new = x_old + eps*fprime
-#	grad_step = eps
-#	while (grad_step < max_eps) { 
-#		if(log_lik_vector(x_new) > log_lik_vector(x_old) ) {
-#			x_new = x_new + delta*c(fprime[1:5],0)
-#			grad_step = grad_step + delta
-#		}
-#		else {break}
-#	}
-#	x_new
-#	max(abs(x_new - x_old))
-	if(max(abs(x_new - x_old)) < precision) {break}
-	print(max(abs(x_new-x_old)))
-	print(iter)
-	x_old = x_new
-}
-x_new
-mle_est = x_new
+print('Finished the Optimization Code')
 
-# Optim Function
+mle_est <- op_llik$par
 
-inits = c(beta, max(sigmasq_0,0.05), max(sigmasq_1,0.05), cov_lambda, theta)
-
-log_lik_vector_2 <- function(params, table1 = Table_1, table2 = Table_2) {
-	return(-log_lik_vector(params, table1, table2))
-}
-
-op_llik <- optim(inits, log_lik_vector_2,lower = c(-Inf, -Inf, 0, 0, 0, 0), upper = c(Inf, Inf, Inf, Inf, 5))
-
-par_values <- op_llik$par
-
-par_values <- c(-0.4952341,  1.0780908,  0.0000000, 12.1815841,  0.4898579,  0.3466315)
-
-
+print(mle_est)
 
 # Compute the Hessian at the MLE estimate
+
+library(numDeriv)
+
+print('Computing the Hessian')
+
+mle_mean = mle_est[1:length(mean_params)]
+mle_cov = , theta
+
 
 obs_inf <- function(log_lik_vector, mle_est) {
 	return(hessian(log_lik_vector, mle_est))	
 }
 
-mle_cov = solve(obs_inf(log_lik_vector, mle_est))
+mle_cov = try(solve(obs_inf(log_lik_vector, mle_est)))
 
-nonzero <- function(x) { return(max(x,0))}
-
-std_error <- function(est_cov) {
-	eig_vals = eigen(-est_cov)$value
-	eig_vectors = eigen(-est_cov)$vectors
-
-	D = diag(as.numeric(lapply(eig_vals,nonzero)))
-	U = eig_vectors
-
-	Est_Param_Cov <- U%*%D%*%t(U)
-
-	est_std_error = sqrt(diag(Est_Param_Cov))
+if (typeof(mle_cov) == 'character') {
+	mle_cov == matrix(nrow = length(mle_est), ncol = length(mle_est))
+	return(list(beta = mle_est[1:length(mean_params)], sigma = mle_est[(length(mean_params)):length(mle_est)], llik = mle_llik, cov = mle_cov))
+}
+else {
+	std_error <- function(mle_cov) {
 	
-	return(est_std_error)
+		eig_vals = eigen(-est_cov)$value
+		eig_vectors = eigen(-est_cov)$vectors
 
+		D = diag(as.numeric(lapply(eig_vals,nonzero)))
+		U = eig_vectors
+
+		Est_Param_Cov <- U%*%D%*%t(U)
+
+		est_std_error = sqrt(diag(Est_Param_Cov))
+	
+		return(est_std_error)
+
+	}
+
+	est_std_error = std_error(mle_est)
+
+	mle_llik <- log_lik_vector(mle_est)
+
+	return(list(beta = mle_est[1:length(mean_params)], sigma = mle_est[(length(mean_params)):length(mle_est)], llik = mle_llik, cov = mle_cov, beta.stderr = est_std_error[1:length(mean_params)], sigma.stderr = est_std_error[(length(mean_params)):length(mle_est)] ))
 }
 
-cbind(mle_est,std_error(mle_cov))
 
-cbind(par_values, std_error(par_cov))
-
+}

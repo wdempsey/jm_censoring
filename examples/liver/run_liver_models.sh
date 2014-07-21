0be15bcb3428565cdbdc1499bc14e184f233d31f
PlusTheMonk <dempsey.walter@gmail.com>
2014-07-21 16:08:23 -0500

    updated run to be argument based depending on weibull or piecewise to be run

--- examples/liver/run_liver_models.sh
+++ examples/liver/run_liver_models.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-Rscript prothrombin_example.R cens
-Rscript prothrombin_example.R int
-Rscript prothrombin_example.R uncens
-Rscript prothrombin_example.R compl
+if [ $1 == 'weibull' ] 
+then
+    echo 'Running All Weibull Fit Code'
+    Rscript prot_ex_weibull.R cens                                                                                                     
+    Rscript prot_ex_weibull.R int
+    Rscript prot_ex_weibull.R uncens
+    Rscript prot_ex_weibull.R compl   
+fi
+
+”if [ $1 == 'piecewise' ]
+then
+    echo 'Running All Piecewise Fit Code'
+    Rscript prot_ex_pwfit.R cens
+    Rscript prot_ex_pwfit.R int
+    Rscript prot_ex_pwfit.R uncens
+    Rscript prot_ex_pwfit.R compl
+fi
+
+
+
+
 
